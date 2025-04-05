078ad9bf53e57fed4d582d6e87ea8c27b196ccfd
Manisha Lohan <manisha1.lohan@gmail.com>
2025-04-05 19:07:35 +0530

    Add files via upload

---
+++ DOM_hits.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from icecube import icetray, dataclasses, clsim, photonics_service, simclasses
+from icecube import dataio
+from glob import glob
+
+files = glob("/home/manisha/t3store3/inice/src/Gen2-Scripts/python/segments/Detector_SIM_Iron_Resample_4_times_lgE_16.9_sin2_0.0/Iron_Trigger_*.i3.gz")
+for filename in files:
+	file = dataio.I3File(filename)
+	print(filename)	
+	while file.more():
+		frame = file.pop_frame()
+		if 'I3RecoPulseSeriesMapGen2' in frame:
+			I3RecoPulseSeriesMapGen2 = frame["I3RecoPulseSeriesMapGen2"]
+			modules = []
+			for OMKey, pulses in I3RecoPulseSeriesMapGen2:
+				if any([p.flags & dataclasses.I3RecoPulse.PulseFlags.LC for p in pulses]):					modules.append((OMKey.string, OMKey.om))
+				n_lc_doms = len(set(modules))
+
+			print(n_lc_doms)
+
+
+
