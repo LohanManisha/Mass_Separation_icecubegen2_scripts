ba7a6393e5527d055acbd81f4233780d71a84142
Manisha Lohan <manisha1.lohan@gmail.com>
2025-04-06 23:02:05 +0530

    Update angular_resolution.py

--- angular_resolution.py
+++ angular_resolution.py
@@ -3,30 +3,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from math import erf
+from scipy import special
+from scipy.special import erf
+
 from icecube import icetray, dataclasses, clsim, photonics_service, simclasses
 from icecube.icetray import I3Units
 from icecube import dataio
 from glob import glob
 
-files = glob("/home/user/inice/src/millipede/resources/examples/hit_cleaning_COG_hits_millipede_Proton_SplineMPE_400m_R_2000ns_T/Proton_millipede_*_SplineMPE.i3.gz")
-for filename in files:
-	file = dataio.I3File(filename)
-	print(filename)
-	while file.more():
-		frame = file.pop_frame()
-		if 'I3MCTree' in frame:
-			I3MCTree = frame['I3MCTree']
-			truth = dataclasses.get_most_energetic_primary(frame['I3MCTree'])
-			#truth1 = dataclasses.get_most_energetic_muon(frame['I3MCTree'])
-			#particle = frame['Fe56Nucleus'] 
-			direction1 = (truth.dir)
-			#direction3 = (truth1.dir)
-#			print(direction1)
-			#print(direction3)
-		if 'SplineMPEOut' in frame:
-			SplineMPEOut = frame['SplineMPEOut']
-			direction2 = (SplineMPEOut.dir)
-#	print(direction2)
-#	print(math.acos(direction1*direction2))
-	print(np.degrees(math.acos(direction1*direction2)))
+##
+##np.loadtxt("proton_hits_beta.txt", unpack = True)
+SIGMA = special.erf(1/np.sqrt(2)) # this is just the exact value of the 0.68....
+angular_resolution = np.percentile((np.loadtxt("Iron_opening_angle.txt", unpack = True)), 100*SIGMA)
+	
+print(angular_resolution, "Iron_resolution_contained_tracks")
+
 
